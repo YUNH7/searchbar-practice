9033de02e7ce22d256db3f2cf15597e55054b29d
YUNH7 <yunny126@gmail.com>
2023-07-17 14:32:19 +0900

    style: 색상 변수, letter-spacing 전역 설정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,6 @@ import { styled } from 'styled-components';
 const TitleStyle = styled.h2`
   font-size: 2.125rem;
   text-align: center;
-  letter-spacing: -0.018em;
   line-height: 1.6;
   white-space: pre-line;
 `;
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,10 +6,11 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;    
     font-family: 'Spoqa Han Sans', 'Sans-serif';
+    letter-spacing: -0.018em;
   }
 
   #root {
-    background-color: rgb(202, 233, 255);
+    background-color: var(--background_blue);
   }
 
   ul, li {
@@ -19,7 +20,19 @@ const GlobalStyle = createGlobalStyle`
   a {
     cursor: pointer;
     text-decoration: none;
-    color: #007BE9;
+    color: var(--main_blue);
+  }
+
+  :root {
+    --main_blue: rgba(0, 123, 233, 1);
+    --background_blue: rgba(202, 233, 255, 1);
+    --white: rgba(255, 255, 255, 1);
+    --black: rgba(0, 0, 0, 1);
+    --gray_700: rgba(147, 155, 146, 1);
+    --gray: rgba(167, 175, 183, 1);
+    --shadow: rgba(167, 175, 183, 0.3);
+    --skyblue: rgba(238, 248, 255, 1);
+    --text_blue: rgba(0, 123, 233, 0.8);
   }
 `;
 
